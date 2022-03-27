c886bcd1ec12106abb88efd277759fc9b2dcc957
Dries Benamar-Belkacem <dries.benamar-belkacem@student.kdg.be>
2022-03-27 16:51:44 +0200

    coins work this time game almost finished maybe add music

--- Assets/Scripts/PickUpCoin.cs
+++ Assets/Scripts/PickUpCoin.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PickUpCoin : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioSource coinSound;
     void Start()
     {
 
@@ -19,6 +20,7 @@ public class PickUpCoin : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            coinSound.Play();
             GameObject.Destroy(this.gameObject);
         }
 
